ad0204cb5120ead39a3bd370738b2a37b18a98f3
tanfulun <swat_tfl@sina.com>
2018-07-05 05:09:45 +0000

    training deepfashion landmark data

--- training/train_cpm.py
+++ training/train_cpm.py
@@ -2,10 +2,10 @@ import os
 import time
 
 # should restart training from snap
-restart_training = False
+restart_training = True
 
 # GPU
-used_gpu = '0,1'
+used_gpu = '0,1,2,3'
 
 # caffe exec
 caffe_exec_path = '/tanfulun/workspaces/Project/caffe-cpm/caffe/build/tools/caffe'
@@ -24,8 +24,8 @@ print(stime_str)
 if not restart_training:
     os.system('nohup %s train --solver=%s -gpu %s >caffe_log.log 2>&1'%(caffe_exec_path,solver_path,used_gpu))
 else:
-    iteration = 500
-    snapshot = project_dir + '/training/prototxt/FLIC/caffemodel/pose_iter_%s.solverstate'%iteration
+    iteration = 6000
+    snapshot = project_dir + '/training/prototxt/DeepFashion_Landmark/caffemodel/landmark_iter_%s.solverstate'%iteration
     os.system('nohup %s train --solver=%s --snapshot=%s -gpu %s >caffe_log_%s.log 2>&1'%(caffe_exec_path,solver_path,snapshot,used_gpu,iteration))
 
 #end-training-time
